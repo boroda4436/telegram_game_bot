9c58a2c7cc3471b02a9e6fce3aa8e122e0ba77e7
Bohdan Chupika <boroda4436@users.noreply.github.com>
2019-04-13 20:42:36 +0300

    TGB-7: Move credentials from DB to application properties (#11)

--- src/main/java/com/cbs/telegram/bot/telegram_api_connector/repository/BotSettingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cbs.telegram.bot.telegram_api_connector.repository;
-
-import com.cbs.telegram.bot.telegram_api_connector.entity.BotSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BotSettingRepository extends JpaRepository<BotSetting, String> {
-
-}
